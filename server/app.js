f1e118c97ee2ea43b6269e767b93e4fd0eafbf66
AKarp123 <ash.daboss111@gmail.com>
2024-08-24 03:15:05 -0400

    environment mode and session secret fix

--- server/app.js
+++ server/app.js
@@ -11,9 +11,12 @@ import { initializeTestData } from "./dbMethods.js";
 import User from "./models/UserModel.js";
 import apiRouter from "./routes/index.js";
 import "dotenv/config";
-import initializeAdmin from "./config/admin.js";
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(
+    process.env.NODE_ENV === "productuin"
+        ? process.env.MONGODB_URI
+        : process.env.MONGODB_DEV_URI
+);
 const db = mongoose.connection;
 
 const app = express();
@@ -28,10 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
-
 app.use(
     session({
-        secret: "super secret",
+        secret: process.env.EXP_SESSION_SECRET,
         resave: false,
         saveUninitialized: false,
         cookie: { maxAge: 3600 * 3 * 1000 },
@@ -40,25 +42,21 @@ app.use(
     })
 );
 
-
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-    
-
-
-
 
 app.use("/api", apiRouter);
 app.use("*", (req, res) => {
     res.sendFile(join(__dirname, "../client/build/index.html"));
-}
-);
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", async function () {
