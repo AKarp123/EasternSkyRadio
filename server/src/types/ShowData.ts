290f3cc0fed2de8f655220e9364fabba81984df2
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2025-08-10 01:50:04 -0400

    fix import 2

--- server/src/types/ShowData.ts
+++ server/src/types/ShowData.ts
@@ -1,5 +1,5 @@
 import { Types } from "mongoose";
-import ISongEntry from "../models/SongEntry.js";
+import { ISongEntry } from "./SongEntry.js";
 
 export interface ShowEntry {
     showId: number;
