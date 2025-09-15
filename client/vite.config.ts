d3998063885a49bb648e324a78f60a9083fba6cf
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2025-09-15 02:27:00 -0400

    lint fix lol

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
-import eslint from "vite-plugin-eslint";
 import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig(() => {
