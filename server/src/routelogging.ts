94e9c425d569b77056d7daee1f821171d7311089
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2025-09-15 17:59:26 -0400

    fix logging

--- server/src/routelogging.ts
+++ server/src/routelogging.ts
@@ -5,11 +5,12 @@ const logRoute = (req: Request, res: Response, next: NextFunction) => {
 	const start = Date.now();
 	res.on('finish', () => {
 		const duration = Date.now() - start;
-		if(process.env.NODE_ENV !== "production" && process.env.NODE_ENV !== "test") {
-			console.log(chalk.hex("#3886b7")(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`)); //eslint-disable-line no-console
-		}
-		if(res.statusCode !== 200 && res.statusCode !== 304 && (process.env.NODE_ENV !== "test" )) {
+		if(process.env.NODE_ENV === "test") return;
+		if(res.statusCode !== 200 && res.statusCode !== 304) {
 			console.error(chalk.hex("#FF7F50")(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`));
+		}
+		else if(process.env.NODE_ENV === "development") {
+			console.log(chalk.hex("#3886b7")(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`)); //eslint-disable-line no-console
 		}
 		
 		
