16b1c5a8ecfcab3e680c010f673fe43d6a9464ab
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2025-09-29 00:14:24 -0400

    add connection

--- server/src/start.ts
+++ server/src/start.ts
@@ -2,17 +2,33 @@ import { app } from "./app.js";
 import { connectToDatabase } from "./db.js";
 import { applyMigrations } from "./migrations.js";
 import initializeApp from "./init.js";
+import { pingServer } from "./subsonic.js";
 
 
 const startServer = async () => {
+	app.locals.subsonicEnabled = process.env.SUBSONIC_ENABLED || false;
+
+
 	try {
 		await connectToDatabase();
 	} catch (error) {
 		console.error("Failed to connect to the database:", error);
 		throw new Error("Database connection failed");
 	}
+	try {
+		if(process.env.SUBSONIC_ENABLED) {
+			await pingServer();
+			app.locals.subsonicEnabled = true;
+		}
+	}
+	catch (error) {
+		console.error("Failed to connect to Subsonic server:", error);
+		app.locals.subsonicEnabled = false;
+	}
 	await initializeApp();
 	await applyMigrations();
+	
+	
 		
 	const port = process.env.PORT || 3000;
 
