7004a5482508ae8ef3a47bbc21a4460be68add11
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2025-10-06 23:39:23 -0400

    fix logging colors

--- server/src/routelogging.ts
+++ server/src/routelogging.ts
@@ -7,10 +7,10 @@ const logRoute = (req: Request, res: Response, next: NextFunction) => {
 		const duration = Date.now() - start;
 		if(process.env.NODE_ENV === "test") return;
 		
-		if(res.statusCode !== 200 && res.statusCode !== 304) {
-			console.error(chalk.hex("#FF7F50")(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`));
-		} else {
+		if([200, 201, 204, 301, 302].includes(res.statusCode)) {
 			console.log(chalk.hex("#3886b7")(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`)); //eslint-disable-line no-console
+		} else {
+			console.error(chalk.hex("#FF7F50")(`${req.method} ${req.originalUrl} ${res.statusCode} - ${duration}ms`));
 		}
 		
 
