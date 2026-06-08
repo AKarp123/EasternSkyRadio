3318f6a2c13949c715810a6f8c7163a504bda4fd
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2026-06-07 19:24:22 -0700

    fix: Passport-local-mongoose module loading issue

--- server/src/models/UserModel.ts
+++ server/src/models/UserModel.ts
@@ -1,16 +1,17 @@
 import { Schema } from "mongoose";
 import mongoose from "mongoose";
-import passportLocalMongoose, { PassportLocalMongooseModel } from "passport-local-mongoose";
+import plmModule, { PassportLocalMongooseModel } from "passport-local-mongoose";
 import { UserDocument } from "../types/User.js";
 
 
+const passportLocalMongoose = typeof plmModule === "function" ? plmModule : plmModule.default;
 
 const UserSchema = new Schema<UserDocument>({
 	username: { type: String, required: true, unique: true },
 	migrated: { type: Boolean }
 });
 
-UserSchema.plugin(passportLocalMongoose as any, {
+UserSchema.plugin(passportLocalMongoose, {
 	iterations: 125_000,
 });
 const User = mongoose.model<UserDocument, PassportLocalMongooseModel<UserDocument>>("User", UserSchema);
